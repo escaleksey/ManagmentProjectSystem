c199d739b36ea62d80b8b6ca56138846f91985b9
Алексей Кушнарев <aakushnarev@edu.hse.ru>
2024-10-29 23:44:40 +0500

    add strutcure

---
+++ manage.py
@@ -0,0 +1,13 @@
+from app import app
+import os
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+
+@app.shell_context_processor
+def make_shell_context():
+  return {'sa': sa, 'so': so}
