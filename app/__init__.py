ed67bf78ea667ac9c335b5d98b8af3b2ea2b339f
Aleksey Kushnarev <scanavik2005@gmail.com>
2024-11-28 12:50:12 +0500

    add model

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 #from .routes import user_routes, auth_routes
+from extentions import db, migrate
 import os
 
 
@@ -7,6 +8,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
+    db.init_app(app)
+    migrate.init_app(app, db)
     # Initialize extensions
     #db.init_app(app)
     #migrate.init_app(app, db)
